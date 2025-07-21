a625c11b6ef610af74532c9325d752f2eb2097b8
Sungket <sungket_rhymes@hotmail.com>
2025-07-21 15:51:28 +0100

    added new files and routes

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,7 +5,7 @@
 
     @foreach ($jobs as $job)
         <li>
-            <a href="/jobs/">
+            <a href="/jobs/{{ $job['id'] }}">
             <strong>{{$job['title']}}</strong>: Pays {{$job['salary']}} per year
             </a>
         </li>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
+Route::get('/jobs', function() {
     return view('jobs', [
         'jobs' => [
             [
@@ -28,11 +29,30 @@ Route::get('/jobs', function () {
     ]);
 });
 
-Route::get('/contact', function ($id) {
-    dd{$id};
-    return view('contact');
+Route::get('/jobs/{id}', function ($id) {
+        $jobs = [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => '$10,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Sales',
+                'salary' => '$25,000'
+            ]
+        ];
+
+        $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+        return view('job', ['job' => $job]);
 });
 
-Route::get('/jobs/{id}', function () {
+Route::get('/contact', function () {
     return view('contact');
 });
